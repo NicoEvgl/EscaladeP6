f96a1e75572d53554c693c56224bf4d4ae0998a3
Nico Evgl <nico.evangelista33@gmail.com>
2020-09-02 16:58:23 +0200

    method createMember on MemberDaoImpl / sql dialect configured (postgresql);

--- escalade-consumer/src/main/java/org/nico/consumer/impl/dao/MemberDaoImpl.java
+++ escalade-consumer/src/main/java/org/nico/consumer/impl/dao/MemberDaoImpl.java
@@ -3,11 +3,27 @@ package org.nico.consumer.impl.dao;
 import org.nico.consumer.contract.dao.MemberDao;
 import org.nico.consumer.impl.AbstractDaoImpl;
 import org.nico.model.beans.Member;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+
+import java.sql.Types;
 
 public class MemberDaoImpl extends AbstractDaoImpl implements MemberDao {
 
     @Override
     public void createMember(Member member) {
+        String sql = "INSERT INTO public.member (gender, first_name, last_name, username, email, password, address, address2, zip, city, role)"
+                + "VALUES (:gender, :firstName, :lastName, :username, :email, :password, :address, :address2, :zip, :city, :role)";
+        NamedParameterJdbcTemplate namedParameterJdbcTemplate = new NamedParameterJdbcTemplate(getDataSourceEscalade());
+        MapSqlParameterSource parameterSource = new MapSqlParameterSource();
+        parameterSource.addValue("gender", member.getGender(), Types.VARCHAR);
+        parameterSource.addValue("firstName", member.getFirstName(), Types.VARCHAR);
+        parameterSource.addValue("lastName", member.getLastName(), Types.VARCHAR);
+        parameterSource.addValue("username", member.getUsername(), Types.VARCHAR);
+        parameterSource.addValue("email", member.getEmail(), Types.VARCHAR);
+        parameterSource.addValue("password", member.getPassword(), Types.VARCHAR);
+        parameterSource.addValue("role", member.getRole(), Types.VARCHAR);
 
+        namedParameterJdbcTemplate.update(sql, parameterSource);
     }
 }
