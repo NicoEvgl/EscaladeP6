4b3f073fef3784e0d57c9ae0cfeea357867379b1
Nico Evgl <nico.evangelista33@gmail.com>
2020-09-02 19:35:45 +0200

    tx manager bean in businessContext.xml / method createMember in MemberManagerImpl completed

--- escalade-business/src/main/java/org/nico/business/impl/manager/MemberManagerImpl.java
+++ escalade-business/src/main/java/org/nico/business/impl/manager/MemberManagerImpl.java
@@ -1,11 +1,23 @@
 package org.nico.business.impl.manager;
 
 import org.nico.business.contract.manager.MemberManager;
+import org.nico.business.impl.AbstractManager;
 import org.nico.model.beans.Member;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionCallbackWithoutResult;
+import org.springframework.transaction.support.TransactionTemplate;
 
-public class MemberManagerImpl implements MemberManager {
+public class MemberManagerImpl extends AbstractManager implements MemberManager {
 
     @Override
     public void createMember(Member member) {
+        TransactionTemplate transactionTemplate = new TransactionTemplate(getPlatformTransactionManager());
+        transactionTemplate.execute(new TransactionCallbackWithoutResult() {
+            @Override
+            protected void doInTransactionWithoutResult(TransactionStatus status) {
+                getDaoFactory().getMemberDao().createMember(member);
+            }
+        });
+
     }
 }
