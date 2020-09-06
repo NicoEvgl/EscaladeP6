88bf781aaef5b13b764200fad608ec7b9746f038
Nico Evgl <nico.evangelista33@gmail.com>
2020-09-06 02:13:23 +0200

    ClimbingSiteManager in ManagerFactory + bean statement in businessContext.xml

--- escalade-business/src/main/java/org/nico/business/contract/ManagerFactory.java
+++ escalade-business/src/main/java/org/nico/business/contract/ManagerFactory.java
@@ -1,9 +1,13 @@
 package org.nico.business.contract;
 
+import org.nico.business.contract.manager.ClimbingSiteManager;
 import org.nico.business.contract.manager.UserManager;
 
 public interface ManagerFactory {
 
     UserManager getUserManager();
     void setUserManager(UserManager userManager);
+
+    ClimbingSiteManager getClimbingSiteManager();
+    void setClimbingSiteManager(ClimbingSiteManager climbingSiteManager);
 }
--- escalade-business/src/main/java/org/nico/business/impl/ManagerFactoryImpl.java
+++ escalade-business/src/main/java/org/nico/business/impl/ManagerFactoryImpl.java
@@ -1,16 +1,22 @@
 package org.nico.business.impl;
 
 import org.nico.business.contract.ManagerFactory;
+import org.nico.business.contract.manager.ClimbingSiteManager;
 import org.nico.business.contract.manager.UserManager;
 
 public class ManagerFactoryImpl implements ManagerFactory {
 
 
     private UserManager userManager;
+    private ClimbingSiteManager climbingSiteManager;
 
     @Override
     public UserManager getUserManager() { return userManager; }
-
     @Override
     public void setUserManager(UserManager userManager) { this.userManager = userManager; }
+
+    @Override
+    public ClimbingSiteManager getClimbingSiteManager() { return climbingSiteManager; }
+    @Override
+    public void setClimbingSiteManager(ClimbingSiteManager climbingSiteManager) { this.climbingSiteManager = climbingSiteManager; }
 }
