0f82201928dbb757d7283d95e6d3d734b9a24dae
Nico Evgl <nico.evangelista33@gmail.com>
2020-09-07 17:19:54 +0200

    method Get displayClimbingSite in ClimbingSiteController

--- escalade-webapp/src/main/java/org/nico/controllers/ClimbingSiteController.java
+++ escalade-webapp/src/main/java/org/nico/controllers/ClimbingSiteController.java
@@ -5,9 +5,11 @@ import org.nico.business.contract.manager.EnumManager;
 import org.nico.business.contract.manager.PhotoManager;
 import org.nico.business.impl.SearchFilter;
 import org.nico.model.beans.ClimbingSite;
+import org.nico.model.beans.Photo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.SessionAttribute;
 
 import javax.inject.Inject;
@@ -25,8 +27,8 @@ public class ClimbingSiteController {
     private PhotoManager photoManager;
 
 
-    @GetMapping(value = "/climbingSiteList")
-    public String displayClimbingSiteList(Model model, @SessionAttribute(value = "memberInSessionId", required = false) Integer memberInSessionId) {
+    @GetMapping("/climbingSiteList")
+    public String displayClimbingSiteList(Model model, @SessionAttribute(value = "userInSessionId", required = false) Integer userInSessionId) {
         List<ClimbingSite> climbingSiteList = climbingSiteManager.findClimbingSiteList();
         List<String> regionList = enumManager.getEnumRegionStringValues();
         List<String> cotationList = enumManager.getEnumCotationStringValues();
@@ -51,5 +53,16 @@ public class ClimbingSiteController {
         model.addAttribute("climbingSiteList", climbingSiteList);
 
         return "climbingSiteList";
+    }
+
+    @GetMapping("/climbingSite/{id}")
+    public String displayClimbingSite(@PathVariable Integer id, @SessionAttribute(value = "userInSessionId", required = false) Integer userInSessionId, Model model){
+        ClimbingSite climbingSite = climbingSiteManager.findClimbingSite(id);
+        List<Photo> photoList = photoManager.findPhotoByClimbingSiteId(id);
+
+        climbingSite.setPhotoList(photoList);
+        model.addAttribute("climbingSite", climbingSite);
+
+        return "climbingSite";
     }
 }
